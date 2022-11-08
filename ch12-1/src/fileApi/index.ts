78af1b39d786297b97212a68d62503e626761b29
kweonsikyung <ekrk6632@naver.com>
2022-11-09 03:24:13 +0900

    csv파일 읽기

--- ch12-1/src/fileApi/index.ts
+++ ch12-1/src/fileApi/index.ts
@@ -7,5 +7,6 @@ import { writeFile} from './writeFile';
 import { readFile } from './readFile'
 import { appendFile } from './appendFile';
 import { deleteFile } from "./deleteFile";
+import { readFileGenerator } from "./readFileGenerator";
 
-export { fileExists, mkdir, rmdir, writeFile, readFile, appendFile, deleteFile}
+export { fileExists, mkdir, rmdir, writeFile, readFile, appendFile, deleteFile, readFileGenerator}
